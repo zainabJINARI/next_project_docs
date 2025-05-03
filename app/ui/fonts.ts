13d104fc4a78c932a8ebbff218ba4ec6d616b48b
zainabjinari <zainabjinari@gmail.com>
2025-05-03 21:51:12 +0100

    second commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,4 @@
+import { Inter } from 'next/font/google';
+export const inter = Inter({ subsets: ['latin']})
+import { Lusitana } from 'next/font/google';
+export const lusitana = Lusitana({ subsets:['latin'], weight: ['400', '700'], variable: '--font-lusitana'})
